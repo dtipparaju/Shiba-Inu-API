5277e999146a5b5a9f0e5a713e6b5d52e4dfd4e8
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-02-27 09:43:49 -0600

    set up structure of content view

--- Shiba Inu API/ContentView.swift
+++ Shiba Inu API/ContentView.swift
@@ -9,18 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack{
+                viewLink(pictureNumber: "1")
+                viewLink(pictureNumber: "2")
+                viewLink(pictureNumber: "3")
+                viewLink(pictureNumber: "4")
+                viewLink(pictureNumber: "5")
+                
+            }
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Categories: Codable {
+    var categories: [String]
+}
+
+struct viewLink: View {
+    var pictureNumber: String
+    var body: some View{
+        NavigationLink("Shiba Inu Picture \(pictureNumber)", destination: EntryView())
+            .padding()
+            .font(.title).bold()
+            .onTapGesture {
+            
+            }
     }
 }
---
+++ Shiba Inu API/EntryView.swift
@@ -0,0 +1,20 @@
+//
+//  EntryView.swift
+//  Shiba Inu API
+//
+//  Created by Dhanush Tipparaju on 2/27/23.
+//
+
+import SwiftUI
+
+struct EntryView: View {
+    var body: some View {
+            Text("Image View")
+    }
+}
+
+struct EntryView_Previews: PreviewProvider {
+    static var previews: some View {
+        EntryView()
+    }
+}
