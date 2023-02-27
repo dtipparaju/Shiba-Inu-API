61c1de1fa57c3087ad316c1c5c821c1500142644
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-02-27 09:22:51 -0600

    Initial Commit

---
+++ Shiba Inu API/Shiba_Inu_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shiba_Inu_APIApp.swift
+//  Shiba Inu API
+//
+//  Created by Dhanush Tipparaju on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Shiba_Inu_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
